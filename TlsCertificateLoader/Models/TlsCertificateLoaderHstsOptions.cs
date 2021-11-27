e5f4fc31f1f29b3c5bf084aa04a0736cbc78b69d
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2021-11-27 12:37:15 +0100

    Fixed summary documentation

--- TlsCertificateLoader/Models/TlsCertificateLoaderHstsOptions.cs
+++ TlsCertificateLoader/Models/TlsCertificateLoaderHstsOptions.cs
@@ -3,11 +3,26 @@ using System;
 
 namespace TlsCertificateLoader.Models
 {
+    /// <summary>
+    /// TlsCertificateLoader middleware for Certbot HSTS options
+    /// </summary>
     public class TlsCertificateLoaderHstsOptions : HstsOptions
     {
+        /// <summary>
+        /// Enable or disable HSTS (Strict-Transport-Security header). Defaults to <see langword="true"/>.
+        /// </summary>
         public bool HstsEnabled { get; set; } = true;
+        /// <summary>
+        /// Sets the max-age parameter of the Strict-Transport-Security header. Defaults to 2 years.
+        /// </summary>
         public new TimeSpan MaxAge { get; set; } = TimeSpan.FromDays(365 * 2);
+        /// <summary>
+        /// <inheritdoc cref="HstsOptions.IncludeSubDomains"/>
+        /// </summary>
         public new bool IncludeSubDomains { get; set; } = false;
+        /// <summary>
+        /// <inheritdoc cref="HstsOptions.Preload"/> Defaults to <see langword="true"/>.
+        /// </summary>
         public new bool Preload { get; set; } = true;
     }
 }
--- TlsCertificateLoader/Models/TlsCertificateLoaderWebOptions.cs
+++ TlsCertificateLoader/Models/TlsCertificateLoaderWebOptions.cs
@@ -2,10 +2,24 @@
 
 namespace TlsCertificateLoader.Models
 {
+    /// <summary>
+    /// TlsCertificateLoader middleware for Certbot web options
+    /// </summary>
     public class TlsCertificateLoaderWebOptions
     {
+        /// <summary>
+        /// The <see cref="System.Net.IPAddress"/> to listen on. Defaults to <see cref="IPAddress.Any"/>.
+        /// </summary>
         public IPAddress IPAddress { get; set; } = IPAddress.Any;
+
+        /// <summary>
+        /// The HTTPS port to listen on. Defaults to 443.
+        /// </summary>
         public int HttpsPort { get; set; } = 443;
+
+        /// <summary>
+        /// The HTTP port to listen on. Defaults to 80.
+        /// </summary>
         public int HttpPort { get; set; } = 80;
     }
 }
