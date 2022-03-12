d8b3be170a86547904fa1e0f0db0612e0ac8f51b
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-03-12 11:41:27 +0100

    Added dependency to NetworkPorts.

--- TlsCertificateLoader/Models/TlsCertificateLoaderWebOptions.cs
+++ TlsCertificateLoader/Models/TlsCertificateLoaderWebOptions.cs
@@ -1,4 +1,5 @@
-﻿using System.Net;
+﻿using NetworkPorts;
+using System.Net;
 
 namespace TlsCertificateLoader.Models
 {
@@ -13,13 +14,13 @@ namespace TlsCertificateLoader.Models
         public IPAddress IPAddress { get; set; } = IPAddress.Any;
 
         /// <summary>
-        /// The HTTPS port to listen on. Defaults to 443.
+        /// The HTTPS port to listen on. Defaults to <see cref="NetworkPort.Https"/>.
         /// </summary>
-        public int HttpsPort { get; set; } = 443;
+        public int HttpsPort { get; set; } = NetworkPort.Https;
 
         /// <summary>
-        /// The HTTP port to listen on. Defaults to 80.
+        /// The HTTP port to listen on. Defaults to <see cref="NetworkPort.Http"/>.
         /// </summary>
-        public int HttpPort { get; set; } = 80;
+        public int HttpPort { get; set; } = NetworkPort.Http;
     }
 }
