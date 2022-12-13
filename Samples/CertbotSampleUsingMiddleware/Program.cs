dec5958d6499bc35f208a16eb29ce5dc41b486ad
Mark Cilia Vincenti <markciliavincenti@gmail.com>
2022-12-13 10:22:21 +0100

    Increased configuration in middleware.

--- Samples/CertbotSampleUsingMiddleware/Program.cs
+++ Samples/CertbotSampleUsingMiddleware/Program.cs
@@ -1,15 +1,23 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Net.Http.Headers;
+using NetworkPorts;
 using System;
 using System.Net;
 using TlsCertificateLoader.Extensions;
 
 var host = Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =>
 {
-    webBuilder.ConfigureServices(services => services.AddTlsCertificateLoader("mydomain.tld", "www.mydomain.tld", "/etc/letsencrypt"));
+    webBuilder.ConfigureServices(services => services.AddTlsCertificateLoader("mydomain.tld", "www.mydomain.tld", "/etc/letsencrypt", o =>
+    {
+        o.IncludeSubDomains = false;
+        o.Preload = true;
+        o.HstsEnabled = true;
+        o.MaxAge = TimeSpan.FromDays(365 * 2);
+    }));
     webBuilder.Configure(configureApp => configureApp.UseTlsCertificateLoader(o =>
     {
         o.UseDefaultFiles = true;
@@ -31,7 +39,14 @@ var host = Host.CreateDefaultBuilder(args).ConfigureWebHostDefaults(webBuilder =
         o.RedirectHttpToHttps = true;
         o.RedirectWwwSubdomainToDomain = true;
     }));
-    webBuilder.UseTlsCertificateLoader(o => o.IPAddress = new IPAddress(0));
+    webBuilder.UseTlsCertificateLoader(o =>
+    {
+        o.HttpPort = NetworkPort.Http;
+        o.HttpsPort = NetworkPort.Https;
+        o.HttpProtocols = HttpProtocols.Http1AndHttp2AndHttp3;
+        o.ListenOnUnencryptedHttp = true;
+        o.IPAddress = new IPAddress(0);
+    });
 }).Build();
 
 await host.RunAsync();
